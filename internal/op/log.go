83df2b7c137fec26310c9750421e6351dea0ad6b
bestruirui <1004279909@qq.com>
2026-07-30 16:40:48 +0800

    fix: :bug: 修复日志清理和清空功能的并发问题

--- internal/op/log.go
+++ internal/op/log.go
@@ -11,6 +11,7 @@ import (
 	"github.com/bestruirui/octopus/internal/model"
 	"github.com/bestruirui/octopus/internal/utils/log"
 	"github.com/bestruirui/octopus/internal/utils/snowflake"
+	"gorm.io/gorm"
 )
 
 var relayLogCache = make([]model.RelayLog, 0, model.DefaultRelayLogFlushSize)
@@ -222,6 +223,8 @@ func relayLogCleanup(ctx context.Context) error {
 	}
 
 	cutoffTime := time.Now().Add(-time.Duration(keepPeriod) * 24 * time.Hour).Unix()
+	relayLogFlushLock.Lock()
+	defer relayLogFlushLock.Unlock()
 	return db.GetDB().WithContext(ctx).Where("time < ?", cutoffTime).Delete(&model.RelayLog{}).Error
 }
 
@@ -293,8 +296,15 @@ func RelayLogList(ctx context.Context, startTime, endTime *int, page, pageSize i
 }
 
 func RelayLogClear(ctx context.Context) error {
+	relayLogFlushLock.Lock()
+	defer relayLogFlushLock.Unlock()
+
+	if err := db.GetDB().WithContext(ctx).Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&model.RelayLog{}).Error; err != nil {
+		return err
+	}
+
 	relayLogCacheLock.Lock()
 	relayLogCache = make([]model.RelayLog, 0, model.DefaultRelayLogFlushSize)
 	relayLogCacheLock.Unlock()
-	return db.GetDB().WithContext(ctx).Where("1 = 1").Delete(&model.RelayLog{}).Error
+	return nil
 }
